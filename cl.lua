36a7a13b6aec6f01ea0a055a542a82e4cc66350a
Lunny <69404314+lunny111@users.noreply.github.com>
2020-12-31 20:08:13 +0300

    Update cl.lua

--- cl.lua
+++ cl.lua
@@ -1,30 +1,34 @@
 RegisterCommand('kartopla', function()
     local ped = PlayerPedId()
-    if not IsPedInAnyVehicle(ped, true) then
-        TriggerEvent('mythic_progbar:client:progress', {
-            name = 'kartopla',
-            duration = 1000,
-            label = 'Kar Toplanıyor..',
-            useWhileDead = false,
-            canCancel = false,
-            controlDisables = {
-                disableMovement = true,
-                disableCarMovement = false,
-                disableMouse = false,
-                disableCombat = true,
-            },
-            animation = {
-                animDict = "anim@mp_snowball",
-                anim = "pickup_snowball",
-            },
-        }, function(status)
-            if not status then
-                TriggerServerEvent('topla', source)
-            end
-        end)
+    local snowlevel = GetSnowLevel()
+    if snowlevel >= 5 then
+        print(snowlevel)
+        if not IsPedInAnyVehicle(ped, true)  then
+            TriggerEvent('mythic_progbar:client:progress', {
+                name = 'kartopla',
+                duration = 1000,
+                label = 'Kar Toplanıyor..',
+                useWhileDead = false,
+                canCancel = false,
+                controlDisables = {
+                    disableMovement = true,
+                    disableCarMovement = false,
+                    disableMouse = false,
+                    disableCombat = true,
+                },
+                animation = {
+                    animDict = "anim@mp_snowball",
+                    anim = "pickup_snowball",
+                },
+            }, function(status)
+                if not status then
+                    TriggerServerEvent('topla', source)
+                end
+            end)
+        else
+            exports['mythic_notify']:SendAlert('error', 'Araç içinde kar toplayamazsın')
+        end
     else
-        exports['mythic_notify']:SendAlert('error', 'Araç içinde kar toplayamazsın')
+        exports['mythic_notify']:SendAlert('error', 'Kar toplanabilecek kadar fazla değil')
     end
 end)
-
--- ^adişş 🔥#5555 ve ortak Lunny#7190
